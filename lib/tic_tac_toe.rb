f52777d8c20d7760f113027c1389758955a6183d
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-24 01:47:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-end 
+end
 
 # The "pipes" || = or. This is saying that if the board returns nil, return an empty array.  So, either display the current board, or a new one.
 
@@ -18,9 +18,9 @@ WIN_COMBINATIONS = [
     [0,4,8],
     [2,4,6]
   ]
-  
+
   #display_board: not the instance variable @board
-  
+
     def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -28,16 +28,16 @@ WIN_COMBINATIONS = [
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
   # input_index
-  
+
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-  
+
   # move: Note that we deleted the boar arguement, and added @ to board.  For instance, #move was move(board, position, char), but now board is intialized, so it is a characteristic of TicTacToe, no need to have it as an argument.  So, #move became simply move(position, char).
-  
-  
+
+
   def move(position, char)
     @board[position] = char
   end
@@ -49,7 +49,7 @@ WIN_COMBINATIONS = [
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
-  
+
 
 #turn_count
   def turn_count
@@ -60,7 +60,7 @@ WIN_COMBINATIONS = [
     end
   end
   return number_of_turns
-  
+
 #current_player
   def current_player
     if turn_count % 2 == 0
@@ -68,8 +68,8 @@ WIN_COMBINATIONS = [
     else
     "O"
   end
- 
-#turn 
+
+#turn
 def turn
     puts "Please enter 1-9:"
     input = gets.strip
